64fd666a7c49b2198db24ae78bf61015e475f7f3
chlgpdus921 <chlgpdus921@naver.com>
2018-03-02 14:46:22 +0900

    BOJ

--- src/realization/BOJ10708.java
+++ src/realization/BOJ10708.java
@@ -1,4 +1,4 @@
-package myPackage;
+package realization;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -9,14 +9,16 @@ public class BOJ10708 {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), SPACE);
+		
 		int n = Integer.parseInt(br.readLine());
 		int m = Integer.parseInt(br.readLine());
 
 		int[] a = new int[m];
-		for (int i = 0; i < a.length; i++) {
+		StringTokenizer st = new StringTokenizer(br.readLine(), SPACE);
+		for (int i = 0; i < m; i++) {
 			a[i] = Integer.parseInt(st.nextToken());
 		}
+		
 		int[][] b = new int[m][n];
 		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine(), SPACE);
@@ -38,12 +40,12 @@ public class BOJ10708 {
 			result[i][a[i] - 1] += n - sum;
 			sum = 0;
 		}
-
+	
 		for (int i = 0; i < n; i++) {
 			int total = 0;
 			for (int j = 0; j < m; j++) {
 				total += result[j][i];
-			}
+			} 
 			System.out.println(total);
 		}
 	}
