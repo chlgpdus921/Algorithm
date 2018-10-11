21862f6f9441653f4455c0e319974909427e636d
chlgpdus921 <chlgpdus921@naver.com>
2018-10-11 22:39:12 +0900

    Level

--- src/myPackage/BOJ10718.java
+++ src/myPackage/BOJ10718.java
@@ -1,10 +1,26 @@
 package myPackage;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BOJ10718 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), "\n");
+		String a = br.readLine();
+		String b = "";
+		String c = " ";
+		c.concat(a+ "\n");
+		System.out.println(c);
+		while (st.hasMoreTokens()) {	
+			st = new StringTokenizer(br.readLine(), "\n");
+			a = br.readLine();
+			b = st.nextToken();
+			c.concat(b + "\n");
+		}
+		System.out.println(c);
 	}
 
 }
