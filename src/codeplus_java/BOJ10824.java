f5b6d639cd5ecf751a8ca716bc75542079fa158d
chlgpdus921 <chlgpdus921@naver.com>
2019-01-03 18:06:01 +0900

    course

--- src/codeplus_java/BOJ10824.java
+++ src/codeplus_java/BOJ10824.java
@@ -1,4 +1,4 @@
-package myPackage;
+package codeplus_java;
 
 import java.io.BufferedReader;
 import java.io.IOException;
---
+++ src/myPackage/BOJ11023.java
@@ -0,0 +1,20 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ11023 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int sum = 0;
+		while(st.hasMoreTokens()){
+			sum += Integer.parseInt(st.nextToken());
+		}
+		System.out.println(sum);
+	}
+
+}
