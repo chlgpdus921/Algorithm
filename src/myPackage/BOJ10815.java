7d8f887e201060d092c76b42e80e15cd2867e745
chlgpdus921 <chlgpdus921@naver.com>
2019-01-03 22:04:59 +0900

    course

---
+++ src/myPackage/BOJ10815.java
@@ -0,0 +1,16 @@
+package myPackage;
+import java.util.*;
+import java.io.*;
+public class BOJ10815 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.valueOf(br.readLine());
+		HashSet <Integer>hs = new HashSet<Integer>(num);
+		//StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for(int i = 0; i<5; i++){
+		int a = Integer.valueOf(br.read());
+		System.out.println(a +"ddd");
+		}
+	}
+
+}
