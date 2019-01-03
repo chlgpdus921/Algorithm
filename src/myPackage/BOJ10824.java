62b7267b9c3602af08c0fffde1e333d7083a896f
chlgpdus921 <chlgpdus921@naver.com>
2019-01-03 17:55:47 +0900

    course

--- src/myPackage/BOJ10824.java
+++ src/myPackage/BOJ10824.java
@@ -1,19 +1,16 @@
-package codeplus_java;
+package myPackage;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BOJ10824 {
-
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String a = br.readLine();
 		String[] line = a.split(" ");
-		int num1 = Integer.parseInt(line[0] + line[1]);
-		int num2 = Integer.parseInt(line[2] + line[3]);
-
+		long num1 = Long.parseLong(line[0] + line[1]);
+		long num2 = Long.parseLong(line[2] + line[3]);
 		System.out.println(num1 + num2);
 	}
 
