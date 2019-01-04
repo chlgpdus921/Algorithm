4359c1d1195dbac0f0d06df6226d6214f06102d4
chlgpdus921 <chlgpdus921@naver.com>
2019-01-04 21:16:59 +0900

    course

--- src/myPackage/BOJ10815.java
+++ src/myPackage/BOJ10815.java
@@ -1,16 +1,26 @@
 package myPackage;
+
 import java.util.*;
 import java.io.*;
-public class BOJ10815 {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int num = Integer.valueOf(br.readLine());
-		HashSet <Integer>hs = new HashSet<Integer>(num);
-		//StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		for(int i = 0; i<5; i++){
-		int a = Integer.valueOf(br.read());
-		System.out.println(a +"ddd");
-		}
-	}
 
+public class BOJ10815 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		boolean result = true;
+		StringBuilder sb = new StringBuilder();
+		int num = Integer.valueOf(br.readLine());
+		HashSet<String> hs = new HashSet<String>(num);
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		while (st.hasMoreTokens()) {
+			hs.add(st.nextToken());
+		}
+		Integer.valueOf(br.readLine());
+		st = new StringTokenizer(br.readLine(), " ");
+		while (st.hasMoreTokens()) {
+			if(hs.contains(st.nextToken()))
+				sb.append(1+" ");
+			else sb.append(0+" ");
+		}
+		System.out.println(sb);
+	}
 }
