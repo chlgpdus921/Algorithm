e183e540cc00f38ec5d17cbaeded4caa49688b59
chlgpdus921 <chlgpdus921@naver.com>
2019-01-11 18:19:40 +0900

    course

---
+++ src/codeplus_basic/BOJ2558.java
@@ -0,0 +1,15 @@
+package codeplus_basic;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ2558 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+
+		System.out.println(a + b);
+	}
+
+}
