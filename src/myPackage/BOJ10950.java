607e10d030d9bc939d06af253e07fab091b9927f
chlgpdus921 <chlgpdus921@naver.com>
2019-01-11 18:26:12 +0900

    course

---
+++ src/myPackage/BOJ10950.java
@@ -0,0 +1,21 @@
+package myPackage;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ10950 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.valueOf(st.nextToken());
+			int b = Integer.valueOf(st.nextToken());
+			sb.append(a + b + "\n");
+		}
+		System.out.println(sb);
+	}
+
+}
