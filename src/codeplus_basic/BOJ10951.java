8f6687a7cbcbf361ddc0a1082b26feb262ff437b
chlgpdus921 <chlgpdus921@naver.com>
2019-01-20 16:50:01 +0900

    course

--- src/codeplus_basic/BOJ10951.java
+++ src/codeplus_basic/BOJ10951.java
@@ -1,20 +1,23 @@
-package myPackage;
+/**
+ * @author: CHOI_HYE_YEON
+ */
+package codeplus_basic;
 
 import java.util.*;
 import java.io.*;
 
+/*
+ * Tip! TC의 갯수가 정해져 있지 않다는 것은 Ctrl+z 으로 프로그램 종료를 의미 
+ * */
 public class BOJ10951 {
 
 	public static void main(String[] args) throws IOException {
 		StringBuilder sb = new StringBuilder();
 		Scanner sc = new Scanner(System.in);
-	//	int a = sc.nextInt();
-	//	int b = sc.nextInt();
-	//	sb.append(a+b +"\n");
 		while (sc.hasNextInt()) {
 			int a = sc.nextInt();
-			 int b = sc.nextInt();
-			sb.append(a+b +"\n");
+			int b = sc.nextInt();
+			sb.append(a + b + "\n");
 		}
 		sc.close();
 		System.out.println(sb);
