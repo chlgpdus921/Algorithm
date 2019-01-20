06e9fe27952cb4f993fe835851c93ac5e71ab8d5
chlgpdus921 <chlgpdus921@naver.com>
2019-01-20 17:00:54 +0900

    course

--- src/myPackage/BOJ10952.java
+++ src/myPackage/BOJ10952.java
@@ -1,10 +1,23 @@
 package myPackage;
 
+import java.util.*;
+import java.io.*;
+
 public class BOJ10952 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException{
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		while(!(a ==0 && b ==0)){
+			sb.append(a+b+"\n");
+			st = new StringTokenizer(br.readLine(), " ");
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+		}
+		System.out.println(sb);
 	}
 
 }
