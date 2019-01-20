7d5dced83f7ef6d005faf59dca805aa1cc09a5fc
chlgpdus921 <chlgpdus921@naver.com>
2019-01-20 17:26:04 +0900

    course

--- src/myPackage/BOJ11021.java
+++ src/myPackage/BOJ11021.java
@@ -1,10 +1,20 @@
 package myPackage;
 
+import java.util.*;
+import java.io.*;
+
 public class BOJ11021 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int n = Integer.valueOf(br.readLine());
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.valueOf(st.nextToken());
+			int b = Integer.valueOf(st.nextToken());
+			sb.append("Case #"+(i+1)+": "+ (a + b) + "\n");
+		}
+		System.out.println(sb);
 	}
-
 }
