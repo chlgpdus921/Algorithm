7d75c82c7da9b4de9f7b7e48f1ab034d284c5bc7
chlgpdus921 <chlgpdus921@naver.com>
2019-01-24 19:20:27 +0900

    course

---
+++ src/codeplus_java/BOJ11718.java
@@ -0,0 +1,16 @@
+package codeplus_java;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ11718 {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNextLine()) {
+			System.out.println(sc.nextLine());
+		}
+
+	}
+
+}
