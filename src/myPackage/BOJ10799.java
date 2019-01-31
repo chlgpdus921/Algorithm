89d910683f19bb6827afdd86443b1fce5d60f4be
chlgpdus921 <chlgpdus921@naver.com>
2019-01-31 17:42:05 +0900

    course

--- src/myPackage/BOJ10799.java
+++ src/myPackage/BOJ10799.java
@@ -6,20 +6,22 @@ import java.util.*;
 public class BOJ10799 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		Stack<Character> sk = new Stack<Character>();
-		int cnt=0;
+		int cnt = 0;
 		String a = br.readLine();
-		for(int i = 0; i< a.length(); i++){
-			if(a.charAt(i) =='('){
+		for (int i = 0; i < a.length(); i++) {
+			if (a.charAt(i) == '(') {
 				sk.push('(');
-				if(a.charAt(i+1) ==')'){
-					cnt++;
-				}
 			}
-			else {
+			else if (a.charAt(i) == ')' && a.charAt(i - 1) == '(') {
 				sk.pop();
+				cnt += sk.size();
+			}
+			else if (a.charAt(i) == ')') {
+				sk.pop();
+				cnt++;
 			}
 		}
+		System.out.println(cnt);
 	}
 }
