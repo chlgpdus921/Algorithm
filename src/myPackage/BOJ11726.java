b9caa489a95f8dcc358f24a05d34b6506c000207
chlgpdus921 <chlgpdus921@naver.com>
2019-02-01 21:00:47 +0900

    course

--- src/myPackage/BOJ11726.java
+++ src/myPackage/BOJ11726.java
@@ -1,10 +1,15 @@
 package myPackage;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class BOJ11726 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int x = Integer.valueOf(br.readLine());
 	}
-
 }
+//n=1 -> 2
+//n=2 -> 2
+//n=3 -> 2 + 4 ->    6°³
+//n=4 -> 
