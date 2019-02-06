8997d6ad7ad98842e51a2ca47f4b0631c18f0f70
chlgpdus921 <chlgpdus921@naver.com>
2019-02-06 22:51:34 +0900

    course

--- src/myPackage/BOJ9095.java
+++ src/myPackage/BOJ9095.java
@@ -1,10 +1,34 @@
 package myPackage;
 
+import java.io.*;
+
 public class BOJ9095 {
+	static int[] d;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int t = Integer.valueOf(br.readLine());
 
+		for (int i = 0; i < t; i++) {
+			int n = Integer.valueOf(br.readLine());
+			d = new int[n + 1];
+			d[0] = 1;
+			sb.append(caculate(n) + "\n");
+		}
+		System.out.println(sb);
+	}
+
+	public static int caculate(int n) {
+		if (d[n] > 0) {
+			return d[n];
+		}
+		else if (n <= 2) {
+			d[n] = n;
+			return d[n];
+		}
+		d[n] = caculate(n - 1) + caculate(n - 2) + caculate(n - 3);
+		return d[n];
 	}
 
 }
