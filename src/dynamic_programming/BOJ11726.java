f38b65048b2be35f7fa03be2fb86abfb5434eef5
chlgpdus921 <chlgpdus921@naver.com>
2019-02-06 19:07:01 +0900

    course

--- src/dynamic_programming/BOJ11726.java
+++ src/dynamic_programming/BOJ11726.java
@@ -1,4 +1,4 @@
-package myPackage;
+package dynamic_programming;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -9,16 +9,17 @@ public class BOJ11726 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int x = Integer.valueOf(br.readLine());
-		d = new int[x+1];
+		d = new int[x + 1];
 		d[0] = 1;
 		d[1] = 1;
 		System.out.println(tile(x));
 	}
-	public static int tile(int x) {//5
+
+	public static int tile(int x) {
 		if (d[x] > 0) {
 			return d[x];
 		}
-		d[x] = ( tile(x-1)+tile(x-2) ) % 10007;//1 00
+		d[x] = (tile(x - 1) + tile(x - 2)) % 10007;
 		return d[x];
 	}
 }
---
+++ src/myPackage/BOJ11727.java
@@ -0,0 +1,10 @@
+package myPackage;
+
+public class BOJ11727 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
