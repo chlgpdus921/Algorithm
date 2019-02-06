ce26743119c2f4620122131ec14d94ba53be93a4
chlgpdus921 <chlgpdus921@naver.com>
2019-02-06 19:24:00 +0900

    course

--- src/myPackage/BOJ11727.java
+++ src/myPackage/BOJ11727.java
@@ -1,10 +1,25 @@
 package myPackage;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class BOJ11727 {
+	static int[] d;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int x = Integer.valueOf(br.readLine());
+		d = new int[x + 1];
+		d[0] = 1;
+		d[1] = 1;
+		System.out.println(tile(x));
+	}
+	public static int tile(int x) {
+		if (d[x] > 0) {
+			return d[x];
+		}
+		d[x] = (2 *tile(x - 2) + tile(x - 1)) % 10007;
+		return d[x];
 	}
 
 }
