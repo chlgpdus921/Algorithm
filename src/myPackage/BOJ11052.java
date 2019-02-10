1d82182771338ddd11dcca46f7308a5ee35b01f0
chlgpdus921 <chlgpdus921@naver.com>
2019-02-10 21:59:26 +0900

    course

--- src/myPackage/BOJ11052.java
+++ src/myPackage/BOJ11052.java
@@ -7,30 +7,25 @@ import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class BOJ11052 {
-	static int[] p;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		p = new int[n + 1];
+		int[] p = new int[n + 1];
+		int[] d = new int[n + 1];
 		int i = 1;
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		while (st.hasMoreTokens()) {
 			p[i] = Integer.valueOf(st.nextToken());
 			i++;
 		}
-		int maxlist = n - 1;// 3
-		list.add(price(1) * n);
 		for (i = 1; i <= n; i++) {
-			list.add(price(i) + price(maxlist));// 1 3 2 2 3 1 4 0
-			maxlist--;
+			for (int j = 1; j <= i; j++) {
+				d[i] = Math.max(d[i], p[j] + d[i - j]);
+				System.out.println(d[i]+"  "+( p[j] + d[i - j]));
+			}
 		}
-		Collections.sort(list);
-		System.out.println(list.get(list.size() - 1));
+		System.out.println(d[n]);
 	}
 
-	public static int price(int e) {
-		return p[e];
-	}
 }
