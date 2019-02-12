f0d9d66a67f6f6cb31b0f6a696cab80ea4c029ef
chlgpdus921 <chlgpdus921@naver.com>
2019-02-12 18:09:33 +0900

    course

--- src/myPackage/BOJ2193.java
+++ src/myPackage/BOJ2193.java
@@ -1,27 +1,20 @@
 package myPackage;
 
-import java.util.*;
 import java.io.*;
+import java.math.BigInteger;
 
 public class BOJ2193 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.valueOf(br.readLine());
-		if (n > 4) {
-			double result= Math.pow(2, n - 4)-1;
-			double result2 = Math.pow(2, n - 3)-1;
-			int a =(int)((int) result + result2);
-			System.out.println(a);
+		BigInteger[] d = new BigInteger[n+1];
+		d[0] = BigInteger.ZERO;
+		d[1] = BigInteger.ONE;
+		for(int i =2; i<=n; i++){
+			d[i] = d[i-1].add(d[i-2]);
 		}
-		else{
-			if(n ==1 || n ==2){
-				System.out.println(1);
-			}
-			else if(n ==3 || n ==4){
-				System.out.println(2);
-			}
-		}
+		System.out.println(d[n]);
 	}
 
 }
