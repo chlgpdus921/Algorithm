fcc9553c3aafeb2c1886fa0da378a9c7d7a8d78b
chlgpdus921 <chlgpdus921@naver.com>
2019-06-28 18:39:23 +0900

    course

--- src/myPackage/BOJ10844.java
+++ src/myPackage/BOJ10844.java
@@ -3,21 +3,33 @@ package myPackage;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class BOJ10844 {
-
-	public static void main(String[] args)throws IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.valueOf(br.readLine());
-		
-		int[][] d = new int[n][9];
-		for(int i = 0; i< n; i++){
-			for(int j = 0; j<9; j++){
-				
-				d[i][j] = d[i+1][j]+ d[i-1][j]; 
+		// i자리 계단수 마지막수 j (0~9)
+		int[][] d = new int[n + 1][10];
+
+		Arrays.fill(d[1], 1);
+	/*	for (int i = 1; i < 10; i++) {
+			d[1][i]++;
+		}*/
+		for (int i = 2; i <= n; i++) {
+			for (int j = 0; j < 10; j++) {
+				if (j == 0)
+					d[i][j] = d[i - 1][1]% 1000000000;
+				else if (j == 9)
+					d[i][j] = d[i - 1][8] % 1000000000;
+				else
+					d[i][j] = (d[i - 1][j - 1] + d[i - 1][j + 1])% 1000000000;
 			}
 		}
-	System.out.println(d[n][]);
+		int sum = 0;
+		for (int j = 0; j < 10; j++) {
+			sum  = (sum + d[n][j]) % 1000000000;
+		}
+		System.out.println(sum % 1000000000);
 	}
-
 }
