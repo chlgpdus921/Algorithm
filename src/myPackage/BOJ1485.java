192ed68a02e56e075c6c71ad9fa737eb32b71fab
chlgpdus921 <chlgpdus921@naver.com>
2019-07-31 20:27:37 +0900

    problem

--- src/myPackage/BOJ1485.java
+++ src/myPackage/BOJ1485.java
@@ -1,5 +1,23 @@
 package myPackage;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BOJ1485 {
+	public static void main(String[] args) throws IOException{
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int t = Integer.valueOf(br.readLine());
+		for(int i = 0; i<t; i++) {
+			for(int j = 0; j<4; j++) {
+				
+			}
+		}
+		
+	}
 
 }
