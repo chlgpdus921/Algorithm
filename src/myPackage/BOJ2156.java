3b488715246b327f2c9a06f9b535483c3ae4eee2
chlgpdus921 <chlgpdus921@naver.com>
2019-08-04 18:25:14 +0900

    UPDATE BOJ2156

---
+++ src/myPackage/BOJ2156.java
@@ -0,0 +1,12 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ2156 {
+	public static void main(String[] args) throws IOException {
+
+	}
+}
