fb6881922f96761af95550ee4d7b0e5a2141479b
chlgpdus921 <chlgpdus921@naver.com>
2019-08-13 17:04:21 +0900

    UPDATE BOJ3376

--- src/myPackage/BOJ14943.java
+++ src/myPackage/BOJ14943.java
@@ -4,20 +4,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-/* 500 -200 -400 50 50* -> 최소 배달 비용 : 950 */
+
+/* 
+ * 500 -200 -400 50 50 -> 최소 배달 비용 : 950 
+ * 누적하여 계산 500 300 -100 -50 0  절댓값으로 sum 계산하기
+ */
 public class BOJ14943 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
+
 		int n = Integer.valueOf(br.readLine());
 		st = new StringTokenizer(br.readLine());
-		int[] d = new int[n+1];
-		int i = 1;
-		while(st.hasMoreTokens()) {
+		long[] d = new long[n];
+		int i = 0;
+		while (st.hasMoreTokens()) {
 			d[i] = Integer.valueOf(st.nextToken());
 			i++;
 		}
-			
+		long sum = Math.abs(d[0]);
+		for (int j = 1; j < n; j++) {
+			d[j] = d[j] + d[j - 1];
+			sum += Math.abs(d[j]);
+		}
+		System.out.println(sum);
 	}
 }
