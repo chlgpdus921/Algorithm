892a677aa26e8a798cb60bd5891667c2959333b6
chlgpdus921 <chlgpdus921@naver.com>
2019-08-17 17:39:22 +0900

    UPDATE BOJ10266

---
+++ src/myPackage/BOJ10266.java
@@ -0,0 +1,13 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ10266 {
+	public static void main(String[] args) throws IOException {
+	
+		
+	}
+}
