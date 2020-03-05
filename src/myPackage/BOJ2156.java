d1dc59b43400ea010fcedd58c5cba3b08a41adf3
Hye-Yeon Choi <chlgpdus921@naver.com>
2020-03-05 21:36:02 +0900

    UPDATE BOJ2156

--- src/myPackage/BOJ2156.java
+++ src/myPackage/BOJ2156.java
@@ -3,46 +3,31 @@ package myPackage;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+
 /*
- * d[i]= i가 선택될때의 최댓값
- *  
- * 
+ * d[i][j] = 총 j개이고, j번째와 연속차이(i)일 때 포도주의 최대 양
  */
 public class BOJ2156 {
 	public static void main(String[] args) throws IOException {
-		StringBuilder sb = new StringBuilder();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.valueOf(br.readLine());
-		int[][] grape = new int[2][n];
-		int[] temp = new int[n];
-		for (int i = 0; i < n; i++) {
-			grape[0][i] = Integer.valueOf(br.readLine());
-			temp[i] = grape[0][i];
+		int[] grape = new int[n + 1];
+		int[] d = new int[n + 1];
+		for (int i = 1; i < n + 1; i++) {
+			grape[i] = Integer.valueOf(br.readLine());
 		}
+		d[0] = 0;
+		d[1] = grape[1];
+		for (int i = 2; i < n + 1; i++) {
+			if (i > 2) {
+				d[i] = Math.max(Math.max(d[i - 1], d[i - 2] + grape[i]), d[i - 3] + grape[i - 1] + grape[i]);
+			}
+			else if(i==2) {
+				d[2] = grape[1] + grape[2];
 
-		Arrays.sort(temp);
-
-		int size = n - 1;
-
-		for (int i = n - 1; i >= 0; i--) {
-			for (int j = 0; j < i; j++) {
-				if (grape[0][j] == temp[i]) { // max값이구
-					if (j > 0 && j < n-1) {
-						if (!(grape[1][j + 1] == 1 && grape[1][j - 1] == 1)) {
-							grape[1][j] = 1;
-
-						}
-					}
-				}
 			}
 		}
-		int sum = 0;
-		for(int i = 0; i<n;i++) {
-			if(grape[1][i] == 1)
-				sum += grape[0][i];
-		}
-		System.out.println(sum);
+		System.out.println(d[n]);
 
 	}
 }
